970fa002f9d2bac2e187102e07655a2fb68bf1f9
0000yuyu <yujin09219@naver.com>
2026-04-28 00:51:12 +0900

    feat : NotificationHistory 엔티티 조립 및 작성

---
+++ src/main/java/com/yeoljeong/tripmate/notification/domain/entity/NotificationHistory.java
@@ -0,0 +1,57 @@
+package com.yeoljeong.tripmate.notification.domain.entity;
+
+import com.yeoljeong.tripmate.domain.BaseAuditEntity;
+import com.yeoljeong.tripmate.notification.domain.constants.ChannelType;
+import com.yeoljeong.tripmate.notification.domain.constants.NotificationType;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Table(name = "p_notification_history")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NotificationHistory extends BaseAuditEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @UuidGenerator
+  private UUID id;
+
+  @Embedded
+  private NotificationEndPoint notificationEndPointSnapShot;
+
+  @Embedded
+  private NotificationSource notificationSource;
+
+  @Embedded
+  private NotificationMessage notificationMessage;
+
+  @Embedded
+  private NotificationPayload notificationPayload;
+
+  @Embedded
+  private NotificationResult notificationResult;
+
+  public void updateResult(NotificationResult notificationResult) {
+    this.notificationResult = notificationResult;
+  }
+
+  protected boolean isFailed() {
+    return notificationResult.isFailed();
+  }
+
+  protected NotificationType getNotificationType() {
+    return this.notificationSource.getType();
+  }
+
+  protected ChannelType getNotificationChannelType() {
+    return this.notificationEndPointSnapShot.getChannelType();
+  }
+}

08a722b4195d62393bd609a84b0db68f43ed1387
0000yuyu <yujin09219@naver.com>
2026-04-27 23:30:47 +0900

    feat : NotificationToken 엔티티 조립 및 작성

---
+++ src/main/java/com/yeoljeong/tripmate/notification/domain/entity/NotificationToken.java
@@ -0,0 +1,41 @@
+package com.yeoljeong.tripmate.notification.domain.entity;
+
+import com.yeoljeong.tripmate.domain.BaseAuditEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Table(name = "p_notification_token")
+@RequiredArgsConstructor
+public class NotificationToken extends BaseAuditEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @UuidGenerator
+  private UUID id;
+
+  @Column(name = "user_id")
+  private UUID userId;
+
+  @Embedded
+  private NotificationEndPoint notificationEndPoint;
+
+  @Embedded
+  private TokenStatus tokenStatus;
+
+  public void updateTokenStatus(TokenStatus tokenStatus) {
+    this.tokenStatus = tokenStatus;
+  }
+
+  public boolean isTokenUsable() {
+    return tokenStatus.isUsable();
+  }
+}

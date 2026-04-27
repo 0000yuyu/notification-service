67a1e193533b4a443df40ce64f06373b81026ff4
0000yuyu <yujin09219@naver.com>
2026-04-27 23:39:45 +0900

    feat : NotificationSetting 엔티티 정의

---
+++ src/main/java/com/yeoljeong/tripmate/notification/domain/entity/NotificationSetting.java
@@ -0,0 +1,34 @@
+package com.yeoljeong.tripmate.notification.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name = "p_notification_setting")
+@RequiredArgsConstructor
+public class NotificationSetting {
+
+  @Id
+  @Column(name = "user_id")
+  private UUID id;
+
+  @Column(nullable = false, name = "is_push_enabled")
+  private boolean pushEnabled;
+
+  private NotificationSetting(UUID userId, boolean pushEnabled) {
+    this.id = userId;
+    this.pushEnabled = pushEnabled;
+  }
+
+  public static NotificationSetting createSetting(UUID user_id) {
+    return new NotificationSetting(user_id, true);
+  }
+
+  public void updatePushEnabled() {
+    this.pushEnabled = !pushEnabled;
+  }
+}

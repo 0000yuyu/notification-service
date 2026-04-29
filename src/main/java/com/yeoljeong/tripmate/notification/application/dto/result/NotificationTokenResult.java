c4f646a9b0081e02e3b518878a8ec6fb03da762e
0000yuyu <yujin09219@naver.com>
2026-04-29 21:53:44 +0900

    refactor : enumType null 방어

--- src/main/java/com/yeoljeong/tripmate/notification/application/dto/result/NotificationTokenResult.java
+++ src/main/java/com/yeoljeong/tripmate/notification/application/dto/result/NotificationTokenResult.java
@@ -2,6 +2,7 @@ package com.yeoljeong.tripmate.notification.application.dto.result;
 
 import com.yeoljeong.tripmate.notification.domain.model.NotificationToken;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import lombok.Builder;
 
 @Builder
@@ -16,8 +17,10 @@ public record NotificationTokenResult(
   public static NotificationTokenResult from(NotificationToken token) {
     return NotificationTokenResult
         .builder()
-        .channelType(String.valueOf(token.getNotificationEndPoint().getChannelType()))
-        .deviceType(String.valueOf(token.getNotificationEndPoint().getDeviceType()))
+        .channelType(token.getNotificationEndPoint().getChannelType().name())
+        .deviceType(
+            Optional.of(token.getNotificationEndPoint().getDeviceType().name()).orElse(null)
+        )
         .deviceId(token.getNotificationEndPoint().getDeviceId())
         .tokenValue(token.getNotificationEndPoint().getTokenValue())
         .updateAt(token.getUpdatedAt())

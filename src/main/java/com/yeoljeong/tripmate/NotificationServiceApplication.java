1806006c466245a301a2853b98d076ac0b05a405
0000yuyu <yujin09219@naver.com>
2026-04-29 15:12:41 +0900

    refactor : 패키지 스캔 어노테이션 제거

--- src/main/java/com/yeoljeong/tripmate/NotificationServiceApplication.java
+++ src/main/java/com/yeoljeong/tripmate/NotificationServiceApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication(scanBasePackages = {
-    "com.yeoljeong.tripmate.notification",
-    "com.yeoljeong.tripmate.common"
-})
+@SpringBootApplication
 public class NotificationServiceApplication {
 
   public static void main(String[] args) {

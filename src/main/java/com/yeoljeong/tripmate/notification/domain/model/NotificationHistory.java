4cbf3f452e209410a8416cc4dd7b15fc6d25e5d3
0000yuyu <yujin09219@naver.com>
2026-05-02 17:41:06 +0900

    feat : isRead 필드 추가

--- src/main/java/com/yeoljeong/tripmate/notification/domain/model/NotificationHistory.java
+++ src/main/java/com/yeoljeong/tripmate/notification/domain/model/NotificationHistory.java
@@ -3,6 +3,7 @@ package com.yeoljeong.tripmate.notification.domain.model;
 import com.yeoljeong.tripmate.domain.BaseAuditEntity;
 import com.yeoljeong.tripmate.notification.domain.constants.ChannelType;
 import com.yeoljeong.tripmate.notification.domain.constants.NotificationType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,31 +12,29 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "p_notification_history")
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NotificationHistory extends BaseAuditEntity {
 
+  @Column(nullable = false)
+  private final boolean isRead = false;
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
-
   private UUID userId;
-
   @Embedded
   private NotificationEndPoint notificationEndPointSnapShot;
-
   @Embedded
   private NotificationSource notificationSource;
-
   @Embedded
   private NotificationMessage notificationMessage;
-
   @Embedded
   private NotificationPayload notificationPayload;
-
   @Embedded
   private NotificationResult notificationResult;
 

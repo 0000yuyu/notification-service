2f617ef5295d4e7416fea642d4d4b6ba2e3e1862
0000yuyu <yujin09219@naver.com>
2026-05-03 17:18:09 +0900

    fix : validated 누락 처리

--- src/main/java/com/yeoljeong/tripmate/notification/presentation/dto/request/NotificationAdminSendRequest.java
+++ src/main/java/com/yeoljeong/tripmate/notification/presentation/dto/request/NotificationAdminSendRequest.java
@@ -2,13 +2,21 @@ package com.yeoljeong.tripmate.notification.presentation.dto.request;
 
 import com.yeoljeong.tripmate.notification.application.dto.command.NotificationAdminSendRequestCommand;
 import com.yeoljeong.tripmate.notification.domain.constants.ChannelType;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
+import org.hibernate.validator.constraints.Length;
 
 public record NotificationAdminSendRequest(
+    @NotNull
     List<UUID> userList,
+    @NotNull
     ChannelType channelType,
+    @NotNull
+    @Length(max = 20)
     String title,
+    @Length(max = 255)
+    @NotNull
     String body
 ) {
 

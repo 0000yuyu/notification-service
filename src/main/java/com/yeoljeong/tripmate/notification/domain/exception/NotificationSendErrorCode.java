f8815baef8b1e0161baf0619a29481c9f2e304eb
0000yuyu <yujin09219@naver.com>
2026-05-08 12:01:39 +0900

    refactor : 에러 코드명 수정

--- src/main/java/com/yeoljeong/tripmate/notification/domain/exception/NotificationSendErrorCode.java
+++ src/main/java/com/yeoljeong/tripmate/notification/domain/exception/NotificationSendErrorCode.java
@@ -13,7 +13,7 @@ public enum NotificationSendErrorCode implements ErrorCode {
   INVALID_NOTIFICATION_FORMAT(HttpStatus.BAD_REQUEST, "알림 메시지 형식이 잘못되었습니다."),
   RATE_LIMIT_EXCEEDED(HttpStatus.TOO_MANY_REQUESTS, "알림 발송 한도를 초과했습니다. 잠시 후 다시 시도해주세요."),
   FCM_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "FCM 서버 일시적 오류로 알림 전송에 실패했습니다."),
-  INTERNAL_NOTIFICATION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알림 시스템 내부 오류가 발생했습니다."),
+  INTERNAL_NOTIFICATION_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "알림 시스템 내부 오류가 발생했습니다."),
   FAILED_LOAD_TEMPLATE(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS, "알림 발송에 필요한 템플릿을 조립하는 데 실패했습니다."),
   ;
   private final HttpStatus status;

c05d2e8450f15112f22ac18041624e9aa2939102
0000yuyu <yujin09219@naver.com>
2026-05-11 12:36:28 +0900

    refactor : 토큰 찾기 함수 쿼리 수정

--- src/main/java/com/yeoljeong/tripmate/NotificationServiceApplication.java
+++ src/main/java/com/yeoljeong/tripmate/NotificationServiceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableDiscoveryClient
 @ConfigurationPropertiesScan
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 @EnableJpaAuditing(auditorAwareRef = "userAuditorAware")
 public class NotificationServiceApplication {
 

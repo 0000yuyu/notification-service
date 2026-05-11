8d965483f31b59b53b98cde06e5d1ea4f17a0789
0000yuyu <yujin09219@naver.com>
2026-05-11 09:45:20 +0900

    refactor : multi, each 발송 로직 함수 구분 및 수정

--- src/main/java/com/yeoljeong/tripmate/notification/application/service/command/NotificationSendService.java
+++ src/main/java/com/yeoljeong/tripmate/notification/application/service/command/NotificationSendService.java
@@ -2,9 +2,10 @@ package com.yeoljeong.tripmate.notification.application.service.command;
 
 import com.yeoljeong.tripmate.exception.BusinessException;
 import com.yeoljeong.tripmate.exception.constants.CommonErrorCode;
+import com.yeoljeong.tripmate.notification.application.dto.command.NotificationSendEachCommand;
+import com.yeoljeong.tripmate.notification.application.dto.command.NotificationSendMulticastCommand;
 import com.yeoljeong.tripmate.notification.application.dto.result.NotificationSendResult;
 import com.yeoljeong.tripmate.notification.application.provider.NotificationSender;
-import com.yeoljeong.tripmate.notification.infrastructure.dto.NotificationSendMessage;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,11 +16,19 @@ public class NotificationSendService {
 
   private final List<NotificationSender> senders;
 
-  public NotificationSendResult send(NotificationSendMessage message) {
+  public NotificationSendResult sendMulticast(NotificationSendMulticastCommand command) {
     NotificationSender sender = senders.stream()
-        .filter(senders -> senders.supports(message.channelType()))
+        .filter(senders -> senders.supports(command.channelType()))
         .findFirst()
         .orElseThrow(() -> new BusinessException(CommonErrorCode.METHOD_NOT_ALLOWED));
-    return sender.send(message);
+    return sender.sendMulticast(command);
+  }
+
+  public NotificationSendResult sendEach(NotificationSendEachCommand command) {
+    NotificationSender sender = senders.stream()
+        .filter(senders -> senders.supports(command.channelType()))
+        .findFirst()
+        .orElseThrow(() -> new BusinessException(CommonErrorCode.METHOD_NOT_ALLOWED));
+    return sender.sendEach(command);
   }
 }
--- src/main/java/com/yeoljeong/tripmate/notification/infrastructure/external/push/FcmNotificationAdapter.java
+++ src/main/java/com/yeoljeong/tripmate/notification/infrastructure/external/push/FcmNotificationAdapter.java
@@ -3,17 +3,20 @@ package com.yeoljeong.tripmate.notification.infrastructure.external.push;
 import com.google.firebase.messaging.BatchResponse;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.MessagingErrorCode;
 import com.google.firebase.messaging.MulticastMessage;
 import com.google.firebase.messaging.Notification;
 import com.google.firebase.messaging.SendResponse;
 import com.yeoljeong.tripmate.exception.BusinessException;
+import com.yeoljeong.tripmate.notification.application.dto.command.NotificationSendEachCommand;
+import com.yeoljeong.tripmate.notification.application.dto.command.NotificationSendMulticastCommand;
+import com.yeoljeong.tripmate.notification.application.dto.command.NotificationSendTarget;
 import com.yeoljeong.tripmate.notification.application.dto.result.NotificationIndividualResult;
 import com.yeoljeong.tripmate.notification.application.dto.result.NotificationSendResult;
 import com.yeoljeong.tripmate.notification.application.provider.NotificationSender;
 import com.yeoljeong.tripmate.notification.domain.constants.ChannelType;
 import com.yeoljeong.tripmate.notification.domain.exception.NotificationSendErrorCode;
-import com.yeoljeong.tripmate.notification.infrastructure.dto.NotificationSendMessage;
 import java.util.List;
 import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
@@ -23,16 +26,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class FcmNotificationAdapter implements NotificationSender {
 
-  private MulticastMessage createMessage(NotificationSendMessage message) {
+  private MulticastMessage createMulticastMessage(NotificationSendMulticastCommand command) {
     return MulticastMessage.builder()
-        .addAllTokens(message.targetTokens())
+        .addAllTokens(command.targetTokens())
         .setNotification(
             Notification
                 .builder()
-                .setTitle(message.title())
-                .setBody(message.body())
+                .setTitle(command.title())
+                .setBody(command.body())
                 .build()
         ).build();
+  }
+
+  private List<Message> createMessageList(List<NotificationSendTarget> targets) {
+    return targets.stream().map(
+        target ->
+            Message.builder()
+                .setNotification(
+                    Notification.builder()
+                        .setTitle(target.title())
+                        .setBody(target.body())
+                        .build())
+                .setToken(target.token())
+                .build()
+    ).toList();
   }
 
   private BatchResponse sendPushMessageForMulticast(MulticastMessage message)
@@ -40,27 +57,47 @@ public class FcmNotificationAdapter implements NotificationSender {
     return FirebaseMessaging.getInstance().sendEachForMulticast(message);
   }
 
+  private BatchResponse sendPushMessageForEach(List<Message> messages)
+      throws FirebaseMessagingException {
+    return FirebaseMessaging.getInstance().sendEach(messages);
+  }
+
+  private NotificationSendResult toResult(BatchResponse batchResponse) {
+    List<SendResponse> responses = batchResponse.getResponses();
+
+    List<NotificationIndividualResult> details = IntStream.range(0, responses.size())
+        .mapToObj(index -> {
+          SendResponse res = responses.get(index);
+          return res.isSuccessful()
+              ? NotificationIndividualResult.success(index)
+              : NotificationIndividualResult.fail(index, res.getException().getMessage());
+        })
+        .toList();
+    return NotificationSendResult.from(details);
+  }
+
   @Override
-  public NotificationSendResult send(NotificationSendMessage sendMessage) {
-    MulticastMessage message = createMessage(sendMessage);
-    if (sendMessage.targetTokens() != null) {
+  public NotificationSendResult sendMulticast(NotificationSendMulticastCommand command) {
+    MulticastMessage message = createMulticastMessage(command);
+    if (command.targetTokens() != null) {
       try {
         BatchResponse response = sendPushMessageForMulticast(message);
-        List<SendResponse> responses = response.getResponses();
-
-        List<NotificationIndividualResult> details = IntStream.range(0, responses.size())
-            .mapToObj(index -> {
-              SendResponse res = responses.get(index);
-              return res.isSuccessful()
-                  ? NotificationIndividualResult.success(index)
-                  : NotificationIndividualResult.fail(index, res.getException().getMessage());
-            })
-            .toList();
-
-        return NotificationSendResult.from(details);
+        return toResult(response);
       } catch (FirebaseMessagingException e) {
         handleFcmException(e);
       }
+    }
+    return null;
+  }
+
+  @Override
+  public NotificationSendResult sendEach(NotificationSendEachCommand command) {
+    List<Message> messages = createMessageList(command.targets());
+    try {
+      BatchResponse response = sendPushMessageForEach(messages);
+      return toResult(response);
+    } catch (FirebaseMessagingException e) {
+      handleFcmException(e);
     }
     return null;
   }

564caf19ccca128e8d515c72cd5988d7fc33400a
0000yuyu <yujin09219@naver.com>
2026-05-11 09:48:51 +0900

    chore : 스케줄러 가능하도록 설정

--- src/main/java/com/yeoljeong/tripmate/NotificationServiceApplication.java
+++ src/main/java/com/yeoljeong/tripmate/NotificationServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableDiscoveryClient
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableScheduling
 @EnableJpaAuditing(auditorAwareRef = "userAuditorAware")
 public class NotificationServiceApplication {
 

02d652967cbb751c4ce63cf24c21f22ba1c8c724
0000yuyu <yujin09219@naver.com>
2026-05-12 16:19:16 +0900

    feat : 모두 읽기 함수 추가

--- src/main/java/com/yeoljeong/tripmate/notification/infrastructure/persistence/jpa/NotificationHistoryJpaRepository.java
+++ src/main/java/com/yeoljeong/tripmate/notification/infrastructure/persistence/jpa/NotificationHistoryJpaRepository.java
@@ -1,11 +1,21 @@
 package com.yeoljeong.tripmate.notification.infrastructure.persistence.jpa;
 
 import com.yeoljeong.tripmate.notification.domain.model.NotificationHistory;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NotificationHistoryJpaRepository extends JpaRepository<NotificationHistory, UUID>,
     JpaSpecificationExecutor<NotificationHistory> {
 
+  @Modifying(clearAutomatically = true)
+  @Query("""
+      UPDATE NotificationHistory n
+      SET n.isRead = true, n.updatedAt = :now
+      WHERE n.userId = :userId AND n.isRead = false
+      """)
+  void markAllAsReadByUserId(UUID userId, LocalDateTime now);
 }

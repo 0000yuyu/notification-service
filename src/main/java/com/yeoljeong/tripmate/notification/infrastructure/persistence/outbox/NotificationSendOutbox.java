b1a765e04f7b80e8025abad1c97989007405e686
0000yuyu <yujin09219@naver.com>
2026-05-12 14:07:43 +0900

    feat : 재발송 관련 엔티티 필드 추가

--- src/main/java/com/yeoljeong/tripmate/notification/infrastructure/persistence/outbox/NotificationSendOutbox.java
+++ src/main/java/com/yeoljeong/tripmate/notification/infrastructure/persistence/outbox/NotificationSendOutbox.java
@@ -3,6 +3,7 @@ package com.yeoljeong.tripmate.notification.infrastructure.persistence.outbox;
 import com.yeoljeong.tripmate.domain.Outbox;
 import com.yeoljeong.tripmate.notification.application.dto.command.NotificationOutboxCommand;
 import com.yeoljeong.tripmate.notification.domain.constants.ChannelType;
+import com.yeoljeong.tripmate.notification.domain.constants.NotificationResultStatus;
 import com.yeoljeong.tripmate.notification.domain.constants.NotificationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,6 +13,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -33,8 +35,6 @@ public class NotificationSendOutbox extends Outbox {
   @Column(nullable = false)
   private UUID tokenId;
 
-  private String failReason;
-
   @Column(nullable = false)
   @Enumerated(EnumType.STRING)
   private NotificationType notificationType;
@@ -43,19 +43,44 @@ public class NotificationSendOutbox extends Outbox {
   @Enumerated(EnumType.STRING)
   private ChannelType channelType;
 
+  private int retryCount;
+  private String reason;
+  private int maxRetryCount;
+  private LocalDateTime nextAttemptAt;
+
+  @Enumerated(EnumType.STRING)
+  private NotificationResultStatus notificationResultStatus;
+
   public static NotificationSendOutbox from
       (NotificationOutboxCommand command) {
     NotificationSendOutbox outbox = new NotificationSendOutbox();
     init(outbox, command.topic(), command.message());
+    outbox.retryCount = 0;
     outbox.historyId = command.historyId();
     outbox.tokenId = command.tokenId();
     outbox.channelType = command.channelType();
     outbox.notificationType = command.notificationType();
+    outbox.nextAttemptAt = LocalDateTime.now();
     return outbox;
   }
 
+  public void skip(String skipReason) {
+    this.notificationResultStatus = NotificationResultStatus.SKIPPED;
+    this.reason = skipReason;
+  }
+
+  public void published() {
+    super.published();
+    this.notificationResultStatus = NotificationResultStatus.PUBLISHED;
+  }
+
   public void fail(String failReason) {
-    super.fail();
-    this.failReason = failReason;
+    ++this.retryCount;
+    this.reason = failReason;
+    this.notificationResultStatus = NotificationResultStatus.FAILED;
+    this.nextAttemptAt = LocalDateTime.now().plusMinutes((long) Math.pow(retryCount, 2));
+    if (this.retryCount >= maxRetryCount) {
+      this.notificationResultStatus = NotificationResultStatus.GIVE_UP;
+    }
   }
 }

f07d3afaf57a04e89094ed7d200fff7ac6272799
0000yuyu <yujin09219@naver.com>
2026-05-14 15:41:45 +0900

    feat : userId로 된 세팅 찾기 함수 추가

--- src/main/java/com/yeoljeong/tripmate/notification/infrastructure/persistence/jpa/NotificationSettingJpaRepository.java
+++ src/main/java/com/yeoljeong/tripmate/notification/infrastructure/persistence/jpa/NotificationSettingJpaRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationSettingJpaRepository extends JpaRepository<NotificationSetting, UUID> {
 
+  void deleteById(UUID id);
 }

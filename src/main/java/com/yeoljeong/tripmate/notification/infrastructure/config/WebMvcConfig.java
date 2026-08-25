b4db2af767c010b60e79819477af16052dd2b5db
yujin <yujin09219@naver.com>
2026-08-25 20:59:57 +0900

    perf : 테스트용

---
+++ src/main/java/com/yeoljeong/tripmate/notification/infrastructure/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.yeoljeong.tripmate.notification.infrastructure.config;
+
+import com.yeoljeong.tripmate.auth.context.LoginUserArgumentResolver;
+import com.yeoljeong.tripmate.auth.context.UserContextInterceptor;
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Primary
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new UserContextInterceptor()).excludePathPatterns(
+        "/auth/login", "/auth/refresh", "/users/signup", "/internal/**", "/actuator/**");
+  }
+
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    resolvers.add(new LoginUserArgumentResolver());
+  }
+}

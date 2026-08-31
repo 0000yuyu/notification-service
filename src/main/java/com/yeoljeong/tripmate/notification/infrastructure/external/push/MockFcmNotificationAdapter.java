102bd05ea7715808f6ed6ad2373c935983ab5a3a
yujin <yujin09219@naver.com>
2026-09-01 01:52:33 +0900

    수정

--- src/main/java/com/yeoljeong/tripmate/notification/infrastructure/external/push/MockFcmNotificationAdapter.java
+++ src/main/java/com/yeoljeong/tripmate/notification/infrastructure/external/push/MockFcmNotificationAdapter.java
@@ -18,8 +18,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class MockFcmNotificationAdapter implements NotificationSender {
 
-  private static final long MIN_DELAY_MS = 80;
-  private static final long MAX_DELAY_MS = 120;
+  private static final long MIN_DELAY_MS = 450;
+  private static final long MAX_DELAY_MS = 550;
 
   @Override
   public NotificationSendResult sendEach(NotificationSendEachCommand command) {
